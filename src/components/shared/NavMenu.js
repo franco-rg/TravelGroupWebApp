16ecf7c85eafaf9211ae7ed2d74d80eb1bc443d7
franco-rg <francordg0@gmail.com>
2022-05-29 11:23:26 -0500

    rfre

--- src/components/shared/NavMenu.js
+++ src/components/shared/NavMenu.js
@@ -1,48 +1,73 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
+import logo from "../../assets/img/logoynombre.svg";
 
 const NavMenu = () => {
-  const [toggle, setToggle] = useState(true);
+    const [toggle, setToggle] = useState(true);
 
-  const toggleNavBar = () => {
-    setToggle(!toggle);
-  };
+    const toggleNavBar = () => {
+        setToggle(!toggle);
+    };
 
-  const [ubicacionActual, setUbicacionActual] = useState(0);
-  window.addEventListener('scroll', function () {
-    setUbicacionActual(window.pageYOffset);
-  })
+    const [ubicacionActual, setUbicacionActual] = useState(0);
+    window.addEventListener('scroll', function () {
+        setUbicacionActual(window.pageYOffset);
+    })
 
-  return (
-    <>
-      <nav className={"slider bg-white m-4 z-50 rounded-xl top-0 left-0 text-white fixed shadow-beautiful transition-all duration-300 ease-in-out active ".concat(toggle ? "" : "active")}>
-        <div className={toggle ? "absolute w-8 top-4 left-4" : "fixed left-2 top-8"}>
-          <button
-            id="btnOpen"
-            onClick={() => {
-              toggleNavBar();
-            }}
-            className={"btn-openMenu sticky top-0 text-2xl cursor-pointer transition-all duration-75 ease-in-out ".concat((ubicacionActual > 50) ? "mostrarBtnNavbar" : "")}>
-            <FontAwesomeIcon
-              icon="fa-solid fa-bars"
-              className="text-gray-600 transition-all duration-300"
+    return (
+        <>
+            <nav
+                className={"slider bg-white m-4 z-50 rounded-xl top-0 left-0 text-white fixed shadow-beautiful transition-all duration-300 ease-in-out active".concat(
+                    toggle ? "" : "active"
+                )}
+            >
+                {/* <div className="flex justify-center flex justify-center bg-gray-100 mt-20" style={{ height: "4rem" }}>
+          <a href="/">
+            <img
+              className="min-w-full my-3"
+              alt="logo_img"
+              src={logo}
+              style={{ maxWidth: "140px" }}
             />
-          </button>
-        </div>
-        <ul className="overflow-hidden nav_list h-3/4 mt-20">
-          <li
-            key="_1"
-            className="py-5 cursor-pointer opacity-90 m-4">
-            <button className="bg-gray-100 w-full py-4 rounded-lg">
-              <div className="ml-2 overflow-hidden w-7">
-                <FontAwesomeIcon icon="fa-solid fa-house" className="text-gray-700" />
-              </div>
-              <span className="uppercase links_name poppins text-gray-700">Inicio</span>
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+          </a>
+        </div> */}
+                <div className="absolute w-8 top-4 left-4">
+                    <button
+                        id="btnOpen"
+                        onClick={() => {
+                            toggleNavBar();
+                        }}
+                        className={"btn-openMenu sticky top-0 text-2xl cursor-pointer transition-all duration-75 ease-in-out ".concat((ubicacionActual > 50) ? "mostrarBtnNavbar" : "")}
+                    >
+                        <FontAwesomeIcon
+                            icon="fa-solid fa-bars"
+                            className="text-gray-600 transition-all duration-300"
+                        />
+                        {/* <FontAwesomeIcon
+              icon="fa-solid fa-xmark"
+              id="btnOpen"
+              className="absolute flex text-gray-600 mx-1 transition-all duration-300 top-2"
+            /> */}
+                    </button>
+                </div>
+                <ul className="overflow-hidden nav_list h-3/4 mt-20">
+                    <li
+                        key="_1"
+                        className="py-5 cursor-pointer opacity-90 m-4"
+                    // onClick={() => {
+                    //   handleClick("/");
+                    // }}
+                    >
+                        <button className="bg-gray-100 w-full py-4 rounded-lg">
+                            <div className="ml-2 overflow-hidden w-7">
+                                <FontAwesomeIcon icon="fa-solid fa-house" className="text-gray-700" />
+                            </div>
+                            <span className="uppercase links_name poppins text-gray-700">Inicio</span>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 };
 export default NavMenu;
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -6,67 +6,67 @@ import ContainerViews from "../../components/shared/ContainerViews";
 import logo from "../../assets/img/nombre.svg";
 
 const Home = () => {
-  return (
-    <>
-      <Header title="Menú principal" />
-      <ContainerViews>
-        <div
-          className="grid grid-cols-11 sm:grid-cols-12 container m-auto gap-5 active mb-5"
-          id="home"
-          data-tab-content
-        >
-          <div className="col-span-5">
-            <div className="rounded-xl overflow-hidden border border-gray-200">
-              <div className="bg-gray-100 px-4 py-2 border">
-                <p className="text-xs text-gray-800 font-medium">
-                  Bienvenido(a)
-                </p>
-                <h1 className="poppins text-lg">Username</h1>
-              </div>
-              <div
-                className="h-48 flex justify-center items-center"
-                style={{ background: "#BAD1CD" }}
-              >
-                <img
-                  className="min-w-full p-28"
-                  src={logo}
-                  alt="logo_img"
-                  style={{ minWidth: "70px", minHeight: "20px" }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex-wrap gap-10">
-            <div className="relative w-80 mb-8 bg-gray-100 rounded-xl h-28 flex justify-center items-center gap-3">
-              <div className="left-8 absolute">
-                <DateTime hora={true} color="#222" />
-                <p className="text-gray-700">Hora Perú</p>
-              </div>
-              <div className="left-56 top-9 absolute">
-                <FontAwesomeIcon
-                  icon="fa-solid fa-clock"
-                  className="text-gray-800 absolute"
-                  style={{ fontSize: "40px" }}
-                />
-              </div>
-            </div>
-            <div className="relative w-80 bg-gray-100 rounded-xl h-28 flex justify-center items-center gap-3">
-              <div className="left-8 absolute">
-                <DateTime fecha={true} color="#222" />
-                <p className="text-gray-700">Fecha Local</p>
-              </div>
-              <div className="left-56 top-9 absolute">
-                <FontAwesomeIcon
-                  icon="fa-solid fa-calendar-days"
-                  className="text-gray-800"
-                  style={{ fontSize: "40px" }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </ContainerViews>
-    </>
-  );
+    return (
+        <>
+            <Header title="Menú principal" />
+            <ContainerViews>
+                <div
+                    className="grid grid-cols-11 sm:grid-cols-12 container m-auto gap-5 active mb-5"
+                    id="home"
+                    data-tab-content
+                >
+                    <div className="col-span-5">
+                        <div className="rounded-xl overflow-hidden border border-gray-200">
+                            <div className="bg-gray-100 px-4 py-2 border">
+                                <p className="text-xs text-gray-800 font-medium">
+                                    Bienvenido(a)
+                                </p>
+                                <h1 className="poppins text-lg">Username</h1>
+                            </div>
+                            <div
+                                className="h-48 flex justify-center items-center"
+                                style={{ background: "#BAD1CD" }}
+                            >
+                                <img
+                                    className="min-w-full p-28"
+                                    src={logo}
+                                    alt="logo_img"
+                                    style={{ minWidth: "70px", minHeight: "20px" }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-wrap gap-10">
+                        <div className="relative w-80 mb-8 bg-gray-100 rounded-xl h-28 flex justify-center items-center gap-3">
+                            <div className="left-8 absolute">
+                                <DateTime hora={true} color="#222" />
+                                <p className="text-gray-700">Hora Perú</p>
+                            </div>
+                            <div className="left-56 top-9 absolute">
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-clock"
+                                    className="text-gray-800 absolute"
+                                    style={{ fontSize: "40px" }}
+                                />
+                            </div>
+                        </div>
+                        <div className="relative w-80 bg-gray-100 rounded-xl h-28 flex justify-center items-center gap-3">
+                            <div className="left-8 absolute">
+                                <DateTime fecha={true} color="#222" />
+                                <p className="text-gray-700">Fecha Local</p>
+                            </div>
+                            <div className="left-56 top-9 absolute">
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-calendar-days"
+                                    className="text-gray-800"
+                                    style={{ fontSize: "40px" }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ContainerViews>
+        </>
+    );
 };
 export default Home;
